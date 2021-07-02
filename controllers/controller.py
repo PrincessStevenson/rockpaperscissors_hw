aef729c536dfc187d4dbe251cd333a1ceced862e
Princess <b.stevenson@hotmail.com>
2021-07-02 15:23:30 +0100

    MVP working & Started css

--- controllers/controller.py
+++ controllers/controller.py
@@ -2,11 +2,18 @@ from flask import render_template
 from app import app
 from models.players import *
 from models.player_list import *
+from models.game import *
+
+
+# @app.route('/')
+# def index():
+    # return "Shall We Play A Little Game.."
 
 
 @app.route('/')
-def index():
-    return "Shall We Play A Little Game.."
+def base():
+    game = Game()
+    return render_template('base.html', title="Shall We Play A Little Game..")
 
 # @app.route('/')
 # def index():
@@ -15,3 +22,18 @@ def index():
 @app.route('/rock/scissors')
 def game1():
     return "Player 1 wins by playing rock!"
+
+
+@app.route('/<player_1_choice>/<player_2_choice>')
+def mvp(player_1_choice, player_2_choice):
+    player_1 = Player("Player 1", player_1_choice)
+    player_2 = Player("Player 2", player_2_choice)
+    game = RockPaper(player_1, player_2)
+    result = game.check_winner(player_1, player_2)
+    if result == "Not a valid choice":
+        return "Not a valid choice"
+    elif result != None:
+        return f"Winner is: {result.name} with {result.choice}"
+    else:
+        return "It is a Draw"
+
--- models/game.py
+++ models/game.py
@@ -1,2 +1,25 @@
-# class Game:
-#     def
+from models.player_list import *
+
+class Game:
+    def __init__(self, player_1=None, player_2=None):
+        self.player_1 = player_1
+        self.player_2 = player_2
+        self.win_dict = {
+            "scissors": "paper",
+            "paper": "rock",
+            "rock": "scissors"
+        }
+        
+    def check_winner(self, player_1, player_2):
+        # if player_1.choice.lower() in self.win_dict.keys() and player_2.choice.lower() in self.win_dict.keys():
+        choice1 = player_1.choice.lower()
+        choice2 = player_2.choice.lower()
+        if self.win_dict.get(choice1) and self.win_dict.get(choice2) != None:
+            if self.win_dict.get(choice1) == choice2:
+                return player_1
+            elif self.win_dict.get(choice2) == choice1:
+                return player_2
+            else:
+                return None
+        else:
+            return "Not a valid choice"
